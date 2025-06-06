cc24010d5300c539c6212afe4bf0f367dc126d2d
Connor Braa <connor@dagger.io>
2025-06-06 14:08:43 -0700

    clean up dagger module, install dagger/dagger/modules/go, use it to build.
    Signed-off-by: Connor Braa <connor@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -1,17 +1,3 @@
-// A generated module for ContainerUse functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
@@ -19,19 +5,21 @@ import (
 	"dagger/container-use/internal/dagger"
 )
 
-type ContainerUse struct{}
-
-// Returns a container that echoes whatever string argument is provided
-func (m *ContainerUse) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+type ContainerUse struct {
+	Source *dagger.Directory
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *ContainerUse) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+// dagger module for building container-use
+func New(
+	//+defaultPath="/"
+	source *dagger.Directory,
+) *ContainerUse {
+	return &ContainerUse{
+		Source: source,
+	}
+}
+
+// Returns a container that echoes whatever string argument is provided
+func (m *ContainerUse) Build(ctx context.Context) *dagger.File {
+	return dag.Go(m.Source).Binary("./cmd/cu")
 }
