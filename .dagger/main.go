bcdadacbb1c2f162680b725f717c91c7475a73c2
Connor Braa <connor@dagger.io>
2025-06-13 16:27:07 -0700

    parameterize properly on org name
    Signed-off-by: Connor Braa <connor@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -40,7 +40,8 @@ func (m *ContainerUse) Release(ctx context.Context,
 	version string,
 	// GitHub token for authentication
 	githubToken *dagger.Secret,
-	// GitHub org name for package publishing
+	// GitHub org name for package publishing, set only if testing release process on a personal fork
+	//+default="dagger"
 	githubOrgName string,
 ) (string, error) {
 	return dag.Goreleaser(m.Source).
