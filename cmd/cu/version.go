f6901556971e1910997149b154bd393a11eff5a0
Connor Braa <connor@dagger.io>
2025-06-13 12:11:23 -0700

    add cu version command
    Signed-off-by: Connor Braa <connor@dagger.io>

---
+++ cmd/cu/version.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Long:  `Print the version, commit hash, and build date of the cu binary.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("cu version %s\n", version)
+		fmt.Printf("commit: %s\n", commit)
+		fmt.Printf("built: %s\n", date)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
